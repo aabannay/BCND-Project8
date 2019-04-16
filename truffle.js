daa52e9bbc1e5212b9f7221a21fa21605525838b
Abdullah Albannay <aabannay@gmail.com>
2019-04-16 18:58:39 +0300

    updated files to run test command

--- truffle.js
+++ truffle.js
@@ -3,17 +3,12 @@ var mnemonic = "candy maple cake sugar pudding cream honey rich smooth crumble s
 
 module.exports = {
   networks: {
-    development: {
-      provider: function() {
-        return new HDWalletProvider(mnemonic, "http://127.0.0.1:8545/", 0, 50);
-      },
-      network_id: '*',
-      gas: 9999999
-    }
+    development: { host: "127.0.0.1", port: 8545, network_id: "*" // Match any network id },
   },
+},
   compilers: {
     solc: {
-      version: "^0.4.24"
+      version: "^0.4.25" 
     }
   }
 };
